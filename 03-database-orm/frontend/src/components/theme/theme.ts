617f8b9284350369cabbf575bd4d3c54295b0f2e
Friox <devfriox@gmail.com>
2025-04-23 15:39:25 +0900

    feat(frontend): 테마컬러 변경 (Gray)

--- 03-database-orm/frontend/src/components/theme/theme.ts
+++ 03-database-orm/frontend/src/components/theme/theme.ts
@@ -5,7 +5,7 @@ import { defaultConfig } from "@chakra-ui/react"
 const config = defineConfig({
   globalCss: {
     html: {
-      colorPalette: "teal", // 기본 색상 팔레트를 변경
+      // colorPalette: "teal", // 기본 색상 팔레트를 변경
     },
   },
   
